a21612609c335ac4d82e2e7cb9b411b0d23cce3d
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-24 08:59:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) if !self.find_by_name(name)
+    return self.create_by_name(name) if !self.find_by_name(name)
+    self.find_by_name(name)
   end
 
-  
+
 
 end
